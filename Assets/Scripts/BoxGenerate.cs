c7fc83056bc593f93e49f269a74be96b29c4c824
ChinCluBi <chincub@gmail.com>
2016-03-07 15:55:29 +0700

    Generated map

---
+++ Assets/Scripts/BoxGenerate.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class BoxGenerate : MonoBehaviour
+{
+	public BoxCollider ground;
+	public Vector3 boxSize;
+
+	int[] numOfBlock = {5, 0};
+	char[,,] map = new [,,]
+	{
+		{
+			{' ', ' ', ' ', ' ', '*'},
+			{' ', ' ', ' ', ' ', '*'},
+			{'*', '*', '*', ' ', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', ' ', '*', '*', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', '*', ' ', '*', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', '*', '*', ' ', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', ' ', ' ', ' ', '*'},
+			{'*', '*', '*', '*', '*'},
+		}
+	};
+
+	// Use this for initialization
+	void Start()
+	{
+		boxSize = ground.size;
+		CreateObstacle();
+	}
+
+	void CreateObstacle()
+	{
+		int currentMap = 0;
+		int widthBlock = map.GetLength (2);
+		int heightBlock = map.GetLength (1);
+		Debug.Log (widthBlock + " " + heightBlock);
+		for(int i=0; i<heightBlock; i++){
+			for(int j=0; j<widthBlock; j++){
+				if (map [currentMap, i, j] == '*') {
+					float y = (boxSize.y * -1) * i;
+					float x = boxSize.x * 3 * j;
+					BoxCollider clone = (BoxCollider)Instantiate (ground, new Vector3 (x, y, 0), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
