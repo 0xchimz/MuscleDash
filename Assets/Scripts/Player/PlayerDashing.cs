f473359f777b330296a5bac55a77b78c0db0ab83
ChinCluBi <chincub@gmail.com>
2016-03-10 11:37:46 +0700

    Dash is done

---
+++ Assets/Scripts/Player/PlayerDashing.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDashing : MonoBehaviour
+{
+	TrailRenderer dashParticles;
+
+	void Awake()
+	{
+		dashParticles = GetComponent<TrailRenderer> ();
+	}
+}
+/*    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+	public float range = 1f;
+
+	public float timeBetweenAttacks = 0.1f;
+	public int attackDamage = 10;
+
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+//    float effectsDisplayTime = 0.2f;
+
+	bool enermyInRange;
+	GameObject enemy;
+	EnemyHealth enemyHealth;
+	Animator anim;
+
+    void Awake ()
+    {
+		enemy = GameObject.FindGameObjectWithTag ("Shootable");
+		enemyHealth = enemy.GetComponent<EnemyHealth> ();
+		Debug.Log (enemyHealth);
+		anim = enemy.GetComponent <Animator> ();
+
+        shootableMask = LayerMask.GetMask ("Shootable");
+        gunParticles = GetComponent<ParticleSystem> ();
+        //gunLine = GetComponent <LineRenderer> ();
+        //gunAudio = GetComponent<AudioSource> ();
+        //gunLight = GetComponent<Light> ();
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot ();
+        }
+
+        if(timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects ();
+        }
+		timer += Time.deltaTime;
+
+		if(timer >= timeBetweenAttacks && enermyInRange && enemyHealth.currentHealth > 0)
+		{
+			Attack ();
+		}
+
+		if(enemyHealth.currentHealth <= 0)
+		{
+			anim.SetTrigger ("Dead");
+		}
+    }
+
+	void Attack ()
+	{
+		timer = 0f;
+
+		if(enemyHealth.currentHealth > 0)
+		{
+			enemyHealth.TakeDamage (attackDamage, transform.forward);
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == enemy)
+		{
+			enermyInRange = true;
+		}
+	}
+
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == enemy)
+		{
+			enermyInRange = false;
+		}
+	}
+
+    public void DisableEffects ()
+    {
+        gunLine.enabled = false;
+        //gunLight.enabled = false;
+    }
+
+
+    void Shoot ()
+    {
+        timer = 0f;
+
+        //gunAudio.Play ();
+
+        //gunLight.enabled = true;
+
+        gunParticles.Stop ();
+        gunParticles.Play ();
+
+        //gunLine.enabled = true;
+        //gunLine.SetPosition (0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+            if(enemyHealth != null)
+            {
+                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+            }
+            //gunLine.SetPosition (1, shootHit.point);
+        }
+        else
+        {
+            //gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
+*/
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,20 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+	public readonly int NORMAL = 0, DASH = 1;
 	public float speed = 6f;
+	public float dashSpeed = 50f;
+
+	public int startingMana = 100;
+	public int currentMana;
+	public Slider manaSlider;
+
+	public float maxTimeDash = 10.0f;
 
 	Vector3 movement;
 	Animator anim;
 	Rigidbody playerRigidbody;
 	int floorMask;
-	float camRayLength = 100f;
+//	float camRayLength = 100f;
+
+	ParticleSystem dashParticles;
 
 	Vector3 offsetAcceleration;
 	Vector3 initialAcceleration;
 
+	public int playerStatus; 
+	float timer;
+
 	void Awake()
 	{
+		currentMana = startingMana;
+		playerStatus = NORMAL;
 		initialAcceleration = Input.acceleration;
 		floorMask = LayerMask.GetMask ("Floor");
 		anim = GetComponent<Animator> ();
@@ -23,28 +41,47 @@ public class PlayerMovement : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		//float h = Input.GetAxisRaw ("Horizontal");
-		//float v = Input.GetAxisRaw ("Vertical");
+		if (currentMana != 100) {
+			currentMana += 1;
+		}
+		manaSlider.value = currentMana;
 
-		offsetAcceleration = Input.acceleration - initialAcceleration;
-		float h = offsetAcceleration.x;
-		float v = offsetAcceleration.y;
-		if (Mathf.Abs(h) < 0.1)
-			h = 0.0f;
-		if (Mathf.Abs(v) < 0.1)
-			v = 0.0f;
-		Move (h, v);
+		Vector3 direction = transform.forward;
+		if (playerStatus == DASH) {
+			if (timer < maxTimeDash) {
+				Move (direction.x, dashSpeed, direction.z);
+				timer += 1f;
+			} else {
+				playerStatus = NORMAL;
+			}
+		} else {
+			if (Input.touchCount > 0 && currentMana == 100) {
+				playerStatus = DASH;
+				timer = 0.0f;
+				Move (direction.x, dashSpeed, direction.z);
+				currentMana = 0;
+			} else {
+				offsetAcceleration = Input.acceleration - initialAcceleration;
+				float h = offsetAcceleration.x;
+				float v = offsetAcceleration.y;
 
-		Turning ();
+				if (Mathf.Abs(h) < 0.1)
+					h = 0.0f;
+				if (Mathf.Abs(v) < 0.1)
+					v = 0.0f;
 
-		Animating (h, v);
+				Move (h, speed, v);
+				Turning ();
+				Animating (h, v);
+			}
+		}
 	}
-	void Move (float h, float v)
+	void Move (float h, float s, float v)
 	{
 		movement.Set (h, 0f, v);
 
-		movement = movement.normalized * speed * Time.deltaTime;
-
+		movement = movement.normalized * s * Time.deltaTime;
+		Debug.Log (movement.normalized);
 		playerRigidbody.MovePosition (transform.position + movement);
 	}
 
